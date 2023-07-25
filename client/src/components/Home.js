0d87c39427f0c4dd602fa8b9269c46900eff9c6a
Robert <rlgallegos85@gmail.com>
2023-07-25 19:53:56 -0400

    dependencies

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,8 @@
 
 
 function Home(){
-    return <div className="bg-blue-200">
-        <h1 className="text-3xl text-blue-700">Hello</h1>
+    return <div>
+        <h1>Home</h1>
     </div>
 }
 export default Home
