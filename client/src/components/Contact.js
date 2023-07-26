3929a5de006bcf213547405fb203d94340793e86
Robert <rlgallegos85@gmail.com>
2023-07-25 20:19:16 -0400

    finished setting up intellisense

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,7 +1,7 @@
 
 
 function Contact(){
-    return <div>
+    return <div className="">
         Contact
     </div>
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,8 @@
 
 
 function Home(){
-    return <div>
-        <h1>Home</h1>
+    return <div className="bg-slate-100">
+        <h1 className="text-white">Home</h1>
     </div>
 }
 export default Home
